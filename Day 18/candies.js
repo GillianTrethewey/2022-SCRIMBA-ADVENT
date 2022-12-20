112a9509f12a7240c1d8ec36a682a9b2db4c75f1
Gillian Trethewey <gilliantrethewey100@gmail.com>
2022-12-20 17:51:05 -0500

    modified day 18

--- Day 18/candies.js
+++ Day 18/candies.js
@@ -1,4 +1,6 @@
+//  3 children have 10 pieces of candy - figure out the total pieces eaten
+
 const candies = (children, candy) => {
-  return Math.floor(candy / children) * children;
+  return parseInt(candy / children) * children;
 };
 console.log(candies(3, 10)); // expected 9
